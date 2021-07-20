bfb9b6a96078b774875da66d62b51da54ab9d35e
olsydor <olsydor@gmail.com>
2021-07-20 03:29:52 +0300

    add first part

--- task1.sh
+++ task1.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+#Helper functions
+function printError() {
+    printf "[${BIRed}ERROR${NC}] $1\n"
+    exit -1
+}
+#only ROOT can run the script
+if [ $UID -eq 0 ]
+then
+echo "You are alowed to run this scenario"
+else
+    printError "You must to be ROOT to run this script"
+fi
